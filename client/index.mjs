dc36997ea13e68e0f3140673a47412e0a4e25ef0
Christopher Robbertse <chrisjrobbertse@gmail.com>
2023-05-07 12:43:19 +0100

    client side javascript changes
     - added more comments
     - added helper functions for
       - calculating age
       - setting max date of birth checker
     - send age in json to server
     - display server json as formatted text to logs

--- client/index.mjs
+++ client/index.mjs
@@ -7,23 +7,68 @@ socket.addEventListener('open', (event) => {
 
 // Listen for messages
 socket.addEventListener('message', (event) => {
+    const jsonObject = JSON.parse(event.data)
+    // Format the data and display it in the HTML element
+    const formattedData = `
+        New employee created<br>
+        Name: ${jsonObject.first_name} ${jsonObject.last_name}<br>
+        Age: ${jsonObject.age}<br>
+        Employment: ${jsonObject.employment}<br><br>
+    `
+
     const output = document.getElementById('output');
-    output.innerHTML += `<p>Received: ${event.data}</p>`;
+
+    output.innerHTML += formattedData
 });
 
-// Send messages to the server
-// document.getElementById('send').addEventListener('click', () => {
-//     const message = document.getElementById('message').value;
-//     socket.send(message);
-// });
+// Function to set the max attribute of the date_of_birth input field to 12 years prior to the current date
+function setDateOfBirthMax() {
+    const today = new Date();
+    const minAge = 12;
+    const maxDate = new Date(today.getFullYear() - minAge, today.getMonth(), today.getDate());
+    const dateString = maxDate.toISOString().split('T')[0];
+    document.getElementById('date_of_birth').max = dateString;
+}
+
+// Call the setDateOfBirthMax function when the page loads
+document.addEventListener('DOMContentLoaded', setDateOfBirthMax);
+
+function calculateAge(dateOfBirth) {
+    const birthDate = new Date(dateOfBirth);
+    const currentDate = new Date();
+    let age = currentDate.getFullYear() - birthDate.getFullYear();
+    const monthDifference = currentDate.getMonth() - birthDate.getMonth();
+
+    if (monthDifference < 0 || (monthDifference === 0 && currentDate.getDate() < birthDate.getDate())) {
+        age--;
+    }
+
+    return age;
+}
 
 function submitForm() {
+    // Get a reference to the form
     const form = document.getElementById('employee_form')
+
+    // Use the FormData API to extract form data
     const formData = new FormData(form)
+
+    // Convert the FormData to a JSON object
     const jsonObject = Object.fromEntries(formData.entries())
+
+    // Update the employment field to be a boolean value
     jsonObject.employment = jsonObject.employment === 'yes';
+
+    // Calculate the age based on the date_of_birth and replace the date_of_birth with age
+    jsonObject.age = calculateAge(jsonObject.date_of_birth);
+    delete jsonObject.date_of_birth;
+
+    // Convert the JSON object to a JSON string
     const jsonString = JSON.stringify(jsonObject)
+
+    // Log the JSON string to the console (for demonstration purposes)
     console.log(jsonString)
 
+    // Send the JSON string to the server
     socket.send(jsonString)
 }
